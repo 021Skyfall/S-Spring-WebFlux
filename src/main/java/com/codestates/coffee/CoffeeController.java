59a1e07dd4dce47e4d90e06e8ef35cd3bd85fe90
021Skyfall <021skyfall@gmail.com>
2023-03-28 15:16:27 +0900

    명칭 변경

--- src/main/java/com/codestates/coffee/CoffeeController.java
+++
@@ -1,5 +0,0 @@
-package com.codestates.coffee;
-
-// TODO CoffeeController 에 Spring WebFlux 를 적용해 주세요. Spring MVC 방식 아닙니다!!
-public class CoffeeController {
-}
---
+++ src/main/java/com/jerry/WebFluxApplication.java
@@ -0,0 +1,17 @@
+package com.jerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+@EnableR2dbcRepositories
+@EnableR2dbcAuditing
+@SpringBootApplication
+public class WebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxApplication.class, args);
+	}
+
+}
